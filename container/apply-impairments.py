a9fa7f5ae0097c6a76ca1b033767d414c83e83a2
jaredoconnell <joconnel@redhat.com>
2021-07-20 14:06:26 +0000

    New config setup and added more configurability
    You can now specify impairments separately for ingress and egress, and specify multiple interfaces

--- container/apply-impairments.py
+++ container/apply-impairments.py
@@ -9,15 +9,25 @@ Variables:
 - START_TIME: The timestamp (epoch) when the impairment is supposed to start.
               Default: Current timestamp at time script is run.
 - END_TIME: The timestamp (epoch) when the impairment will end. Overrides duration.
-- INTERFACE: The interface to apply impairments. Default: ens1f1
-- LATENCY: Latency in ms to apply. 0 to disable. Default 0.
-- PACKET_LOSS: Percent packet loss (0-100). 0 to disable. Default 0.
-- BANDWIDTH_LIMIT: The bandwidth limit in kbits. 0 to disable. Default 0.
-- IMPAIRMENT_DIRECTION: ingress, egress, or both. Default egress.
-                        Uses ifb for ingress impairments (a kernel module).
+- INTERFACE: Comma separated list of interfaces to apply impairments. Default: ens1f1
+- LATENCY: Bidirectional latency in ms to apply. Round-trip latency is double.
+                        0 to disable. Default 0.
+- PACKET_LOSS: Bidirectional percent packet loss (0-100). 0 to disable. Default 0.
+- BANDWIDTH_LIMIT: Bidirectional bandwidth limit in kbits. 0 to disable. Default 0.
 - LINK_FLAPPING: Whether to turn on and off the interface. (True/False) Default: False
 - LINK_FLAP_DOWN_TIME: Time period to flap link down (Seconds)  Default: 2
 - LINK_FLAP_UP_TIME: Time period to flap link down (Seconds). Default: 2
+
+Directional impairment variables:
+    Overrides the bidirecational options above.
+    Uses ifb for ingress impairments (a kernel module).
+    0 to disable. Default 0.
+- EGRESS_LATENCY: Outbound latency.
+- EGRESS_PACKET_LOSS: Outbound packet loss.
+- EGRESS_BANDWIDTH_LIMIT: Outbound packet loss.
+- INGRESS_LATENCY: Inbound latency.
+- INGRESS_PACKET_LOSS: Inbound packet loss.
+- INGRESS_BANDWIDTH_LIMIT: Inbound packet loss.
 """
 
 
@@ -87,18 +97,16 @@ def command(cmd, dry_run, cmd_directory="", mask_output=False, mask_arg=0, no_lo
     sys.exit(1)
   return return_code, output
 
-def parse_tc_netem_args():
+def parse_tc_netem_args(prefix = "", args = {}):
   """
   Uses the environment variables to construct an array of params for netem
 
   Currently supports latency, packet loss, and bandwidth limit as documented
   in the module docstring.
   """
-  args = {}
-
-  latency_evar = int(os.environ.get("LATENCY", 0))
-  packet_loss_evar = float(os.environ.get("PACKET_LOSS", 0))
-  bandwidth_limit_evar = int(os.environ.get("BANDWIDTH_LIMIT", 0))
+  latency_evar = int(os.environ.get(prefix + "LATENCY", 0))
+  packet_loss_evar = float(os.environ.get(prefix + "PACKET_LOSS", 0))
+  bandwidth_limit_evar = int(os.environ.get(prefix + "BANDWIDTH_LIMIT", 0))
   if latency_evar > 0:
     args["latency"] = ["delay", "{}ms".format(latency_evar)]
   if packet_loss_evar > 0:
@@ -138,29 +146,34 @@ def remove_tc_netem(interfaces, dry_run=False, ignore_errors=False):
       logger.error("Removing latency and packet loss failed, tc rc: {}".format(rc))
       sys.exit(1)
 
-def setup_ifb(interface, dry_run):
+def setup_ifb(interfaces, dry_run):
   logger.info("Setting up ifb interface")
-  command(["modprobe", "ifb"], dry_run, fail_on_error=True)
-  command(["ip", "link", "set", "dev", "ifb0", "up"], dry_run, fail_on_error=True)
-  command(["tc", "qdisc", "add", "dev", interface, "ingress"], dry_run, fail_on_error=True)
-  command(["tc", "filter", "add", "dev", interface, "parent", "ffff:",
+  command(["modprobe", "ifb", "numifbs=" + str(len(interfaces))], dry_run, fail_on_error=True)
+  for i, interface in enumerate(interfaces):
+    new_interface = "ifb" + str(i)
+    command(["ip", "link", "set", "dev", new_interface, "up"], dry_run, fail_on_error=True)
+    command(["tc", "qdisc", "add", "dev", interface, "ingress"], dry_run, fail_on_error=True)
+    command(["tc", "filter", "add", "dev", interface, "parent", "ffff:",
             "protocol", "ip", "u32", "match", "u32", "0", "0", "flowid",
-            "1:1", "action", "mirred", "egress", "redirect", "dev", "ifb0"],
+            "1:1", "action", "mirred", "egress", "redirect", "dev", new_interface],
             dry_run, fail_on_error=True)
 
-def remove_ifb(interface, dry_run):
+def remove_ifb(interfaces, dry_run):
   logger.info("Removing IFB")
-  command(["tc", "qdisc", "del", "dev", interface, "ingress"], dry_run)
-  command(["ip", "link", "set", "dev", "ifb0", "down"], dry_run)
+  for i, interface in enumerate(interfaces):
+    new_interface = "ifb" + str(i)
+    command(["tc", "qdisc", "del", "dev", interface, "ingress"], dry_run)
+    command(["ip", "link", "set", "dev", new_interface, "down"], dry_run)
   command(["modprobe", "-r", "ifb"], dry_run)
 
-def set_flap_links(interface, up, dry_run, ignore_errors=False):
+def set_flap_links(interfaces, up, dry_run, ignore_errors=False):
   logger.info("Flapping links " + up)
-  ip_command = ["ip", "link", "set", interface, up]
-  rc, _ = command(ip_command, dry_run)
-  if rc != 0 and not ignore_errors:
-    logger.error("RWN workload, ip link set {} {} rc: {}".format(interface, up, rc))
-    sys.exit(1)
+  for interface in interfaces:
+    ip_command = ["ip", "link", "set", interface, up]
+    rc, _ = command(ip_command, dry_run)
+    if rc != 0 and not ignore_errors:
+      logger.error("RWN workload, ip link set {} {} rc: {}".format(interface, up, rc))
+      sys.exit(1)
 
 def main():
 
@@ -174,24 +187,31 @@ def main():
 
   # Now, the impairments
 
-  netem_impairments = parse_tc_netem_args()
+  bidirectional_netem_impairments = parse_tc_netem_args()
+  ingress_netem_impairments = parse_tc_netem_args("INGRESS_", bidirectional_netem_impairments.copy())
+  egress_netem_impairments = parse_tc_netem_args("EGRESS_", bidirectional_netem_impairments.copy())
   duration = int(os.environ.get("DURATION", -1)) # Seconds
   start_time = int(os.environ.get("START_TIME", time.time())) # Epoch
   end_time = int(os.environ.get("END_TIME", -1)) # Epoch
-  inbound_interface = os.environ.get("INTERFACE", "ens1f1")
+  egress_interfaces = os.environ.get("INTERFACE", "ens1f1").split(",")
   dry_run = os.environ.get("DRY_RUN", "false").lower() == "true"
   flap_links = os.environ.get("LINK_FLAPPING", "false").lower() == "true"
   link_flap_down = int(os.environ.get("LINK_FLAP_DOWN_TIME", 1))
   link_flap_up = int(os.environ.get("LINK_FLAP_UP_TIME", 1))
-  impairment_direction = os.environ.get("IMPAIRMENT_DIRECTION", "egress").lower()
 
   if end_time == -1:
     if duration == -1:
       duration = 60
     end_time = start_time + duration
 
-  if len(netem_impairments) or flap_links:
+  has_netem_impairments = (len(ingress_netem_impairments) + len(egress_netem_impairments)) != 0
+
+  if has_netem_impairments or flap_links:
     logger.info("Running impairments")
+    if len(ingress_netem_impairments):
+      logger.info("Running ingress impairments")
+    if len(egress_netem_impairments):
+      logger.info("Running egress impairments")
   else:
     logger.warn("No impairments. Exiting")
     return
@@ -207,33 +227,37 @@ def main():
     time.sleep(.1)
     current_time = time.time()
 
-  if len(netem_impairments):
-    interfaces = []
-    if impairment_direction != "ingress":
-      interfaces.append(inbound_interface)
-    if impairment_direction != "egress":
-      interfaces.append("ifb0")
+  if has_netem_impairments:
+    ingress_interfaces = []
+    if len(egress_netem_impairments):
+      for i in range(len(egress_interfaces)):
+        ingress_interfaces.append("ifb" + str(i))
+    all_interfaces = egress_interfaces + ingress_interfaces
 
     # Remove, just in case.
     remove_tc_netem(
-      interfaces,
+      all_interfaces,
       dry_run,
       True)
-    remove_ifb(inbound_interface, dry_run)
+    remove_ifb(egress_interfaces, dry_run)
 
-    if impairment_direction != "egress":
-      setup_ifb(inbound_interface, dry_run)
+    if len(ingress_netem_impairments):
+      setup_ifb(egress_interfaces, dry_run)
 
     apply_tc_netem(
-        interfaces,
-        netem_impairments,
+        ingress_interfaces,
+        ingress_netem_impairments,
+        dry_run)
+    apply_tc_netem(
+        egress_interfaces,
+        egress_netem_impairments,
         dry_run)
   else:
     logger.info("No netem impairments")
 
   if flap_links:
     link_flap_count = 1
-    set_flap_links(inbound_interface, "down", dry_run)
+    set_flap_links(egress_interfaces, "down", dry_run)
     next_flap_time = time.time() + link_flap_down
     links_down = True
 
@@ -243,12 +267,12 @@ def main():
       if current_time >= next_flap_time:
         if links_down:
           links_down = False
-          set_flap_links(inbound_interface, "up", dry_run)
+          set_flap_links(egress_interfaces, "up", dry_run)
           next_flap_time = time.time() + link_flap_up
         else:
           links_down = True
           link_flap_count += 1
-          set_flap_links(inbound_interface, "down", dry_run)
+          set_flap_links(egress_interfaces, "down", dry_run)
           next_flap_time = time.time() + link_flap_down
 
     time.sleep(.1)
@@ -262,16 +286,16 @@ def main():
     logger.warn("Ending early due to pod/system termination")
 
   if flap_links:
-    set_flap_links(inbound_interface, "up", dry_run, True)
+    set_flap_links(egress_interfaces, "up", dry_run, True)
 
-  if len(netem_impairments):
+  if has_netem_impairments:
     # Done
     remove_tc_netem(
-      interfaces,
+      all_interfaces,
       dry_run)
 
-    if impairment_direction != "egress":
-      remove_ifb(inbound_interface, dry_run)
+    if len(ingress_netem_impairments):
+      remove_ifb(egress_interfaces, dry_run)
 
   # Sleep until ended
   while running:
